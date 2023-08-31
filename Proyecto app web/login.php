4953f0715dbe5a02062dc02d4781fb6c98ea41b5
Josue Andres Brazales Cango <112193488+JABRAZALAES@users.noreply.github.com>
2023-08-31 17:14:48 -0500

    Commits

--- Proyecto app web/login.php
+++ Proyecto app web/login.php
@@ -1,36 +1,93 @@
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    $file = "users.txt"; // Ruta al archivo de texto
-
-    // Leer el archivo línea por línea
-    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-    foreach ($lines as $line) {
-        list($storedUsername, $storedPassword) = explode(":", $line);
-
-        if ($storedUsername === $username && $storedPassword === $password) {
-            $_SESSION["username"] = $username;
-            header("Location: dashboard.php");
-            exit();
-        }
-    }
-
-    $error = "Credenciales incorrectas";
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Error de inicio de sesión</title>
+    <title>Iniciar sesión</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            background: url('FOOTER/fondo2.png') no-repeat center center fixed;
+            background-size: cover;
+            font-family: 'Bebas Neue', sans-serif; /* Aplica la fuente de Google */
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+
+        .card {
+            background: rgba(255, 255, 255, 0.9);
+        }
+
+        .btn-yellow {
+            background-color: rgb(242, 242, 38);
+            color: black;
+        }
+        .btn-link{
+            color: black;
+        }
+    </style>
 </head>
 <body>
-    <p><?php echo $error ?? ''; ?></p>
-    <a href="login.html">Volver al formulario de inicio de sesión</a>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <!-- Agrega aquí el logo de tu página -->
+                <center><img src="FOOTER/img/logo_white_large.png" alt="Logo"></center>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="username">Nombre de usuario:</label>
+                        <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Contraseña:</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <?php
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            $username = $_POST["username"];
+                            $password = $_POST["password"];
+
+                            $file = "users.txt"; // Ruta al archivo de texto
+
+                            // Leer el archivo línea por línea
+                            $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+                            $validCredentials = false;
+
+                            foreach ($lines as $line) {
+                                list($storedUsername, $storedPassword) = explode(":", $line);
+
+                                if ($storedUsername === $username && $storedPassword === $password) {
+                                    $validCredentials = true;
+                                    break;
+                                }
+                            }
+
+                            if ($validCredentials) {
+                                session_start();
+                                $_SESSION["username"] = $username;
+                                header("Location: dashboard.php");
+                                exit();
+                            } else {
+                                echo '<div class="alert alert-danger" role="alert">Credenciales incorrectas</div>';
+                            }
+                        }
+                    ?>
+                    <button type="submit" class="btn btn-yellow">Iniciar sesión</button>
+                </form>
+                <div class="mt-3">
+                    <a href="register.html" class="btn btn-link">¿No tienes una cuenta? Regístrate aquí</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- Proyecto app web/register.php
+++ Proyecto app web/register.php
@@ -1,46 +1,102 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $confirm_password = $_POST["confirm_password"];
-
-    // Verificar si las contraseñas coinciden
-    if ($password !== $confirm_password) {
-        $error = "Las contraseñas no coinciden";
-    } else {
-        // Verificar si el usuario ya existe en el archivo
-        $file = "users.txt";
-        $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-        foreach ($lines as $line) {
-            list($storedUsername, $storedPassword) = explode(":", $line);
-
-            if ($storedUsername === $username) {
-                $error = "El usuario ya está registrado";
-                break;
-            }
-        }
-
-        if (!isset($error)) {
-            // Agregar el nuevo usuario al archivo
-            file_put_contents($file, "$username:$password\n", FILE_APPEND);
-            $success = "Usuario registrado exitosamente";
-        }
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Resultado del registro</title>
+    <title>Registrarse</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background: url('FOOTER/fondo2.png') no-repeat center center fixed;
+            background-size: cover;
+            font-family: 'Bebas Neue', sans-serif; 
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 40px;
+            background-color: rgba(255, 255, 255, 0.8);
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            margin-top: 100px;
+        }
+
+        .form-group label {
+            font-weight: bold;
+        }
+
+        .btn-primary {
+            background-color: #ddeb19;
+            border-color: #007bff;
+        }
+    </style>
 </head>
 <body>
-    <?php if (isset($error)) { ?>
-        <p>Error: <?php echo $error; ?></p>
-    <?php } elseif (isset($success)) { ?>
-        <p>Éxito: <?php echo $success; ?></p>
-    <?php } ?>
-    <a href="register.html">Volver al formulario de registro</a>
+    <div class="container">
+        <center><img src="FOOTER/img/logo_white_large.png" alt="Logo" class="mb-4"></center>
+        <h2 class="text-center mb-4">Registrarse</h2>
+        
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $confirm_password = $_POST["confirm_password"];
+
+            // Verificar si las contraseñas coinciden
+            if ($password !== $confirm_password) {
+                $error = "Las contraseñas no coinciden";
+            } else {
+                // Verificar si el usuario ya existe en el archivo
+                $file = "users.txt";
+                $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+                foreach ($lines as $line) {
+                    list($storedUsername, $storedPassword) = explode(":", $line);
+
+                    if ($storedUsername === $username) {
+                        $error = "El usuario ya está registrado";
+                        break;
+                    }
+                }
+
+                if (!isset($error)) {
+                    // Agregar el nuevo usuario al archivo
+                    file_put_contents($file, "$username:$password\n", FILE_APPEND);
+                    $success = "Usuario registrado exitosamente";
+                }
+            }
+        }
+        ?>
+
+        <form action="register.php" method="post">
+            <div class="form-group">
+                <label for="username">Nombre de usuario:</label>
+                <input type="text" class="form-control" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Contraseña:</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="confirm_password">Confirmar contraseña:</label>
+                <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Registrarse</button>
+            <div class="text-center mt-3">
+                <a href="login.html">Volver a iniciar sesión</a>
+            </div>
+        </form>
+
+        <?php if (isset($error)) { ?>
+            <div class="alert alert-danger mt-3" role="alert">
+                <?php echo $error; ?>
+            </div>
+        <?php } elseif (isset($success)) { ?>
+            <div class="alert alert-success mt-3" role="alert">
+                <?php echo $success; ?>
+            </div>
+        <?php } ?>
+    </div>
 </body>
 </html>
